d13cf96c4725c0dea7386af163beb25024cfd859
Heyther Vergara <vheyther@gmail.com>
2023-04-24 18:15:44 +0000

    git push

--- Programas/python/ciclo_de_vida.py
+++ Programas/python/ciclo_de_vida.py
@@ -3,18 +3,18 @@ edad_como_cadena = input("Dime tu edad: ")
 edad = int(edad_como_cadena)
 
 if edad >= 0 and edad <= 2:
-    print("Bebé")
+    print("Eres un Bebé")
 elif edad >= 3 and edad <= 11:
-    print("Niño")
+    print("Eres un Niño")
 elif edad >= 12 and edad <= 17:
     print("Adolescente")  
 elif edad >= 18 and edad <= 34:
     print("Adulto")
 elif edad == 35:
     print(" Felicidades Te llamas Máximo Décimo Meridio, comandante de los ejércitos del norte, general de las legiones fénix, leal servidor del verdadero emperador Marco Aurelio, padre de un hijo asesinado, marido de una mujer asesinada, y alcanzaras tu venganza, en esta vida o en la otra.") 
-elif edad >=36 <= 64:
+elif edad >=36 and edad <= 64:
     print("Adulto")      
-elif edad >= 65:
+elif edad >= 65 and edad<= 114:
     print("Anciano")
-else:
-    print("Edad inválida")
+elif edad >= 115:
+    print("Eres una Momia")
--- Programas/python/lalala.py
+++ Programas/python/lalala.py
@@ -1,5 +1,23 @@
-porcentaje_aumento = 30
-valor_original = 100
-descuento = valor_original * (porcentaje_aumento / 100)
-valor_con_descuento = valor_original - descuento
-print(f"El valor con descuento es {valor_con_descuento}")
+edad_como_cadena = input ("Hola, y bienvenido al programa de Ciclo de vida. Por favor, ingrese su edad (en numeros): ")
+Nedad = int(edad_como_cadena) 
+
+if Nedad>=0 and Nedad<=5:
+    print ("Su edad es ", Nedad,". Usted esta en la primera infancia.")
+elif Nedad>=6 and Nedad<=11: 
+    print ("Su edad es ", Nedad,". Usted esta en la infancia.")
+elif Nedad>=12 and Nedad<=18: 
+    print ("Su edad es ", Nedad,". Usted esta en la adolescencia.")
+elif Nedad>=18 and Nedad<=26:
+    print ("Su edad es ", Nedad,". Usted es un joven adulto.")
+elif Nedad>=27 and Nedad<=34:
+    print ("Su edad es ", Nedad,". Usted esta en la adultez.")
+elif Nedad==35:
+    print ("su nombre es Maximo Decimo Meridio, comandante de los ejercitos del norte, general de las legiones fenix, leal serivido del verdadero emperador Marco Aurelio, padre de un hijo asesinado, marido de una mujer asesinada, y alcanzaras tu venganza en esta vida o en la otra.")
+elif Nedad>=36 and Nedad<=59:
+    print ("Su edad es ", Nedad,". Usted esta en la adultez.")
+elif Nedad>=60 and Nedad<=100: 
+    print ("Su edad es ", Nedad,". Usted esta en la vejez.")
+elif Nedad>=101 and Nedad<=39999: 
+    print ("Su edad es ", Nedad,". Seguramente lograste encontrar el secreto de la casi inmortalidad.")
+elif Nedad>=40000 and Nedad<=49999: 
+    print ("Su edad es ", Nedad,". Seguramente haces parte...DEL GRAN IMPERIO DE LA HUMANIDAD LIDERADO POR EL TODOPODEROSO Y GRAN EMPERADOR HENRY CAVILL.")
